4d2c501edc02a44b1a197646fb01d8157d390302
kzielinski96 <kornel.zielinski96@gmail.com>
2019-04-10 16:16:48 +0200

    Added basic CRUD operations

--- EF - miniproject/MainWindow.xaml.cs
+++ EF - miniproject/MainWindow.xaml.cs
@@ -17,9 +17,6 @@ using System.Data.Entity;
 
 namespace EF___miniproject
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         Entities context = new Entities();
@@ -37,5 +34,56 @@ namespace EF___miniproject
             context.Table_1.Load();
             tabViewSource.Source = context.Table_1.Local;
         }
+
+        private void AddCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            var newRecord = new Table_1()
+            {
+                city_name = city_nameTextBox.Text,
+                city_id = city_idTextBox.Text,
+                temperature = double.Parse(temperatureTextBox.Text),
+            };
+            context.Table_1.Add(newRecord);
+            context.SaveChanges();
+            tabViewSource.View.Refresh();
+        }
+
+        private void UpdateCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            var cur = tabViewSource.View.CurrentItem as Table_1;
+
+            if (city_nameTextBox.Equals(""))
+            {
+                cur.city_name = city_nameTextBox.Text;
+            }
+
+            if (city_idTextBox.Equals(""))
+            {
+                cur.city_id = city_idTextBox.Text;
+            }
+
+            if (temperatureTextBox.Equals(""))
+            {
+                cur.temperature = double.Parse(temperatureTextBox.Text);
+            }
+
+            context.SaveChanges();
+            tabViewSource.View.Refresh();
+        }
+
+        private void DeleteCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            var cur = tabViewSource.View.CurrentItem as Table_1;
+
+            if (cur != null)
+            {
+                context.Table_1.Remove(cur);
+            }
+
+            context.SaveChanges();
+            tabViewSource.View.Refresh();
+        }
     }
+
+
 }
