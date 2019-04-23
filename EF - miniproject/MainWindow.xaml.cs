91e3bd737ed80a756a1ba1befcd830da28ee1295
kzielinski96 <kornel.zielinski96@gmail.com>
2019-04-23 16:40:48 +0200

    Added functionality that enables adding remotely data to the database from the Web Api & fixed some minor bugs associated with CRUD operations

--- EF - miniproject/MainWindow.xaml.cs
+++ EF - miniproject/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.Entity;
+using System.Net;
+using System.Threading;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 namespace EF___miniproject
@@ -22,11 +27,12 @@ namespace EF___miniproject
         Entities context = new Entities();
         CollectionViewSource tabViewSource;
 
+
         public MainWindow()
         {
             InitializeComponent();
             tabViewSource = ((CollectionViewSource) (FindResource("table_1ViewSource")));
-
+            GetApiData();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -43,30 +49,25 @@ namespace EF___miniproject
                 city_id = city_idTextBox.Text,
                 temperature = double.Parse(temperatureTextBox.Text),
             };
-            context.Table_1.Add(newRecord);
-            context.SaveChanges();
-            tabViewSource.View.Refresh();
+
+                context.Table_1.Add(newRecord);
+                context.SaveChanges();
+                tabViewSource.View.Refresh();
         }
 
         private void UpdateCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
             var cur = tabViewSource.View.CurrentItem as Table_1;
+            var cur1 = table_1DataGrid.SelectedItem as Table_1;
 
-            if (city_nameTextBox.Equals(""))
+            if (cur.id == cur1.id)
             {
                 cur.city_name = city_nameTextBox.Text;
-            }
-
-            if (city_idTextBox.Equals(""))
-            {
                 cur.city_id = city_idTextBox.Text;
-            }
-
-            if (temperatureTextBox.Equals(""))
-            {
                 cur.temperature = double.Parse(temperatureTextBox.Text);
             }
 
+            context.Entry(cur).CurrentValues.SetValues(cur);
             context.SaveChanges();
             tabViewSource.View.Refresh();
         }
@@ -83,7 +84,55 @@ namespace EF___miniproject
             context.SaveChanges();
             tabViewSource.View.Refresh();
         }
+
+
+        private async void GetApiData()
+        {
+            Random random = new Random();
+            string apiKey = "747fe0e760b5276cc648effd30d3a2a8";
+            string apiBaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+            await Task.Run(() =>
+            {
+                using (var client = new WebClient())
+                {
+                    while (true)
+                    {
+                        double lat = random.Next(35, 71);
+                        double lon = random.Next(-9, 68);
+
+                        string apiCall = apiBaseUrl + "?lat=" + lat + "&lon=" + lon + "&apikey=" + apiKey +
+                                         "&mode=json&units=metric";
+
+                        string jsonString = client.DownloadString(apiCall);
+                        var jsonObject = JsonConvert.DeserializeObject<JObject>(jsonString);
+                        string j_name = jsonObject["name"].ToString();
+                        string j_id = jsonObject["id"].ToString();
+                        string j_temp = jsonObject["main"]["temp"].ToString();
+
+                        if (j_name == "")
+                        {
+                            j_name = "Ocean or See";
+                        }
+
+                        var newRecord = new Table_1()
+                        {
+                            city_name = j_name,
+                            city_id = j_id,
+                            temperature = double.Parse(j_temp),
+                        };
+
+                        Dispatcher.Invoke(() =>
+                        {
+                            context.Table_1.Add(newRecord);
+                            context.SaveChanges();
+                            tabViewSource.View.Refresh();
+                        });
+
+                        Thread.Sleep(5000);
+                    }
+                }
+            });
+        }
     }
-
-
 }
